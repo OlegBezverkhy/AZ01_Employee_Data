bab862d458fab573057ef3cccfa10bee55878a93
OlegBezverkhy <124727891+OlegBezverkhy@users.noreply.github.com>
2024-08-11 16:39:56 +0300

    Update part2.py

--- part2.py
+++ part2.py
@@ -6,5 +6,5 @@ df = pd.read_csv(data_file)
 # Получаем представление о данных по первым 5 строкам
 print('Первые пять строк таблицы:')
 print(df.head(5))
-
+print('Средняя заработная плата(Salary) по городам')
 print(df.groupby('City')['Salary'].mean())
